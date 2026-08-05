1c9dc07cac9a42c9d20e6beefeca6ef62684ec92
Chardie Gotis <chardiegotis2003@gmail.com>
2026-08-05 10:25:05 +0800

    Fix signed‑in redirect

--- app/sync/page.tsx
+++ app/sync/page.tsx
@@ -1,6 +1,7 @@
 import { getOrSyncUser, isAdminOrSuperAdmin } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
 export default async function SyncPage() {
   const dbUser = await getOrSyncUser();
 
