b84fd417d3035af15d9027a13a7bf6a6b2f98651
Chardie Gotis <chardiegotis2003@gmail.com>
2026-08-05 10:18:13 +0800

    Fix signed‑in redirect

--- proxy.ts
+++ proxy.ts
@@ -11,15 +11,7 @@ export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
   const { pathname, searchParams } = req.nextUrl;
 
-  // If user is already signed in and visits /sign-in (excluding /sign-in/sso-callback)
-  if (userId && pathname.startsWith('/sign-in') && !pathname.startsWith('/sign-in/sso-callback')) {
-    const redirectUrlParam = searchParams.get('redirect_url');
-    let targetPath = '/sync';
-    if (redirectUrlParam && redirectUrlParam.startsWith('/') && !redirectUrlParam.startsWith('//')) {
-      targetPath = redirectUrlParam;
-    }
-    return NextResponse.redirect(new URL(targetPath, req.url));
-  }
+
 
   // Protect all non-public routes
   if (!isPublicRoute(req) && !userId) {
